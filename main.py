a112bfce6a4a90c52dd3f6b092a5bc61d799a56d
KaraivanOK <porohvernivk@gmail.com>
2023-08-27 23:21:31 +0300

    Після цього можна в файлі `main.py` замінити `https://example.com/path/to/page` на `127.0.0.1:5000/` - выполено.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ def parse(query: str) -> dict:
 
 
 if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('127.0.0.1:5000/?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('127.0.0.1:5000/?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}

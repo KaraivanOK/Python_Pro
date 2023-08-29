6cdce8177c5c2d45bfd11cb160eac1cfa1b8515b
KaraivanOK <porohvernivk@gmail.com>
2023-08-29 17:24:03 +0300

    Реалізувати функцію parse (див. модуль main.py), Написати 10 тестів до неї (4). Створити Пул реквест в гілку мастер з розв'язанням.

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
+import requests
+
+
 def parse(query: str) -> dict:
-    return {}
+    response = requests.get(query)
+    return response.json()
 
 
 if __name__ == '__main__':
-    assert parse('127.0.0.1:5000/?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('127.0.0.1:5000/?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('http://example.com/') == {}
-    assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('http://127.0.0.1:5000/?name=ferret&color=purple') == {
+        'name': 'ferret', 'color': 'purple'}
+    assert parse('http://127.0.0.1:5000/?name=ferret&color=purple&') == {
+        'name': 'ferret', 'color': 'purple'}
+    assert parse('http://127.0.0.1:5000?') == {}
+    assert parse('http://127.0.0.1:5000?') == {}
+    assert parse('http://127.0.0.1:5000?name=Dima') == {'name': 'Dima'}
+    assert parse('http://127.0.0.1:5000?name=Dima') == {'name': 'Dima'}
+    assert parse('http://127.0.0.1:5000?name=Dima&age=28') == {'name': 'Dima',
+                                                               'age': '28'}
+    assert parse('http://127.0.0.1:5000?name=Dima&age=28') == {'name': 'Dima',
+                                                               'age': '28'}
+    assert parse('http://127.0.0.1:5000?name=Dima=User&age=28') == {
+        'name': 'Dima=User', 'age': '28'}
+    assert parse('http://127.0.0.1:5000?name=Dima=User&age=28') == {
+        'name': 'Dima=User', 'age': '28'}
 
 
-def parse_cookie(query: str) -> dict:
-    return {}
 
-
-if __name__ == '__main__':
-    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-    assert parse_cookie('') == {}
-    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}

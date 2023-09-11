4aafbb4cbf752c88c47194b457a162e891d2d60c
KaraivanOK <porohvernivk@gmail.com>
2023-09-11 23:49:06 +0300

    Удалили все локальные ветки кроме мастер.

---
+++ HW1/main.py
@@ -0,0 +1,21 @@
+def parse(query: str) -> dict:
+    return {}
+
+
+if __name__ == '__main__':
+    assert parse('127.0.0.1:5000/?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('127.0.0.1:5000/?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parse('http://example.com/') == {}
+    assert parse('http://example.com/?') == {}
+    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+
+
+def parse_cookie(query: str) -> dict:
+    return {}
+
+
+if __name__ == '__main__':
+    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
+    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}

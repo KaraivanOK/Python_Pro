5f6382da8ba27cb087eee40e262ee4231425a303
KaraivanOK <porohvernivk@gmail.com>
2023-10-03 16:46:00 +0300

    Изменены имена переменных и логика метода contains.

--- HW5/Circle.py
+++ HW5/Circle.py
@@ -1,37 +1,20 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 class Circle:
     def __init__(self, x, y, radius):
         self.x = x
         self.y = y
         self.radius = radius
 
-    def contains(self, point):
-        x1 = point[0]
-        y1 = point[1]
-        if self.x <= x1 <= self.x + self.radius and \
-                self.y <= y1 <= self.y + self.radius:
-            return True
-        elif self.x <= x1 <= self.x + self.radius and \
-                self.y >= y1 >= self.y - self.radius:
-            return True
-        elif self.x >= x1 >= self.x - self.radius and \
-                self.y >= y1 >= self.y - self.radius:
-            return True
-        elif self.x >= x1 >= self.x - self.radius and \
-                self.y <= y1 <= self.y + self.radius:
-            return True
-        else:
-            return False
+    def contains(self, point) -> bool:
+        return (point.x - self.x) ** 2 + (
+                point.y - self.y) ** 2 <= self.radius ** 2
 
 
-class Point:
-    def __init__(self, x1, y1):
-        self.x1 = x1
-        self.y1 = y1
-
-    def point(self):
-        return self.x1, self.y1
-
-
-krug = Circle(0, 0, 10)
-tochka = Point(1, 1)
-print(krug.contains(tochka.point()))
+my_circle = Circle(0, 0, 10)
+my_point = Point(8, 8)
+print(my_circle.contains(my_point))
